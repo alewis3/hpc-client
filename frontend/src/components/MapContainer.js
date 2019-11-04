ae74990e61ff5da5fd92c88d2504859f0b085911
Evangeline Mavrommati <eisis@stedwards.edu>
2019-11-04 13:33:04 -0600

    clickable markers

--- frontend/src/components/MapContainer.js
+++ frontend/src/components/MapContainer.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,  //Hides or the shows the infoWindow
+    activeMarker: {},          //Shows the active marker upon click
+    selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+  };
+
+  onMarkerClick = (props, marker, e) =>
+  this.setState({
+    selectedPlace: props,
+    activeMarker: marker,
+    showingInfoWindow: true
+  });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return (
       <Map
@@ -11,9 +33,33 @@ class MapContainer extends Component {
         initialCenter={{ lat: 30.266666, lng: -97.733330}}
       >
         <Marker position={{ lat: 30.2297, lng: -97.7539}}
-                icon={{url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"}} />
+                name={'Homeowner'}
+                onClick={this.onMarkerClick}
+                icon={{url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"}} 
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
         <Marker position={{lat: 30.2477, lng: -97.7181}}
-                icon={{url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}} />
+                name={'Business Owner'}
+                onClick={this.onMarkerClick}
+                icon={{url: "http://maps.google.com/mapfiles/ms/icons/purple-dot.png"}} 
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
       </Map>
     );
   }
@@ -23,6 +69,7 @@ export default GoogleApiWrapper({
   apiKey: 'AIzaSyA21oeXAPbop6Qh1YCvpK9oNrSSqwCVsaY'
 }) (MapContainer);
 
+// add style heere to mimic 'container'
 const mapStyles = {
   width: '100%',
   height: '100vh',
