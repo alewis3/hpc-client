fe7457c5a820cf3944602a749682d90d0c8d0a5b
Evangeline Mavrommati <eisis@stedwards.edu>
2019-11-12 14:54:10 -0600

    prohibited & allowed items

---
+++ frontend/src/components/AllowedItems.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
+import TextField from '@material-ui/core/TextField';
+import axios from 'axios';
+import theme from '../theme';
+import { makeStyles } from '@material-ui/core/styles';
+
+export default function AllowedItems() {
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <TextField
+        id="outlined-multiline-static"
+        label="Multiline"
+        multiline
+        rows="10"
+        defaultValue="Default Value"
+        className={classes.textField}
+        margin="normal"
+        variant="outlined"
+      />
+      <br />
+      <br />
+      <Button
+        variant="contained"
+        color="primary"
+      >Save</Button>
+    </ThemeProvider>
+  )
+}
+
+const useStyles = makeStyles(theme => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 500,
+  },
+}));
--- frontend/src/components/HostPreferences.js
+++ frontend/src/components/HostPreferences.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { AppBar, Button } from '@material-ui/core';
 import theme from '../theme';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
-import { spacing } from '@material-ui/system';
 import { makeStyles } from '@material-ui/core/styles';
+import AllowedItems from './AllowedItems';
+import ProhibitedItems from './ProhibitedItems'
 
 export default function HostPreferences() {
   const classes = useStyles();
@@ -20,19 +18,13 @@ export default function HostPreferences() {
           <Grid item xs={6}>
             <Paper className={classes.paper}>
               <h4>Prohibited Items</h4>
-              <Button
-                variant="contained"
-                color="primary"
-              >Save</Button>
+              <ProhibitedItems />
             </Paper>
           </Grid>
           <Grid item xs={6}>
             <Paper className={classes.paper}>
               <h4>Allowed Items</h4>
-              <Button
-                variant="contained"
-                color="primary"
-              >Save</Button>
+              <AllowedItems />
             </Paper>
           </Grid>
         </Grid>
