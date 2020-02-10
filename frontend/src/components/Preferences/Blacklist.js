2f57f361fe1a81dc144fe2ef957b8d811abeb7c7
emavro <eisis@stedwards.edu>
2020-02-10 17:51:24 -0600

    shell components

---
+++ frontend/src/components/Preferences/Blacklist.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import axios from 'axios';
+import theme from '../../theme';
+
+class Blacklist extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  // componentDidMount() {
+  //   // fetch blacklisted users
+  // }
+
+  // render() {
+  //   return (
+
+  //   )
+  // }
+}
+
+export default Blacklist;
---
+++ frontend/src/components/Preferences/BlockUsers.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+import axios from 'axios';
+import theme from '../../theme';
+
+class BlockUsers extends Component {
+  constructor() {
+    super(props);
+  }
+
+  // render() {
+  //   return (
+
+  //   )
+  // }
+}
+
+export default BlockUsers;
